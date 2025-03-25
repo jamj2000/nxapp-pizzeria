951bac49b6a223ae665dbbe97b2c0f931bfa778a
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-03-25 15:25:50 +0100

    ...

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,11 +3,15 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="h-screen grid place-content-center place-items-center gap-4">
-            <div className="text-9xl">🍕</div>
-            <Link href="/repartidores" className="block text-5xl font-bold">REPARTIDORES</Link>
-            <Link href="/pedidos" className="block text-5xl font-bold">PEDIDOS</Link>
-            <Link href="/pizzas" className="block text-5xl font-bold">PIZZAS</Link>
+        <div className="h-screen grid md:grid-cols-2 place-content-center place-items-center gap-4">
+            <div className="text-9xl">
+                🍕
+            </div>
+            <div>
+                <Link href="/repartidores" className="block text-4xl font-bold">REPARTIDORES</Link>
+                <Link href="/pedidos" className="block text-4xl font-bold">PEDIDOS</Link>
+                <Link href="/pizzas" className="block text-4xl font-bold">PIZZAS</Link>
+            </div>
         </div>
     );
 }
