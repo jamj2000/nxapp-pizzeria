e36ef6a12b061e576a1c06e239d2ed19bb673cda
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2026-03-03 13:36:47 +0100

    refactor store

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,45 +1,43 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-export const useStore = create(
-    persist(
-        (set, get) => ({
-            cart: [],
-            addToCart: (pizza) => set((state) => {
-                const productInCart = state.cart.find((item) => item.id === pizza.id)
-                if (productInCart) {
-                    return {
-                        cart: state.cart.map((item) =>
-                            item.id === pizza.id
-                                ? { ...item, quantity: item.quantity + 1 }
-                                : item
-                        ),
-                    }
-                } else {
-                    return { cart: [...state.cart, { ...pizza, quantity: 1 }] }
-                }
-            }),
-            increaseQuantity: (id) => set((state) => ({
+const store = (set, get) => ({
+    cart: [],
+    addToCart: (pizza) => set((state) => {
+        const productInCart = state.cart.find((item) => item.id === pizza.id)
+        if (productInCart) {
+            return {
                 cart: state.cart.map((item) =>
-                    item.id === id
+                    item.id === pizza.id
                         ? { ...item, quantity: item.quantity + 1 }
                         : item
                 ),
-            })),
-            decreaseQuantity: (id) => set((state) => ({
-                cart: state.cart.map((item) =>
-                    item.id === id && item.quantity > 1
-                        ? { ...item, quantity: item.quantity - 1 }
-                        : item
-                ),
-            })),
-            removeFromCart: (id) => set((state) => ({
-                cart: state.cart.filter((item) => item.id !== id)
-            })),
-            clearCart: () => set({ cart: [] }),
-        }),
-        {
-            name: 'cart-storage',
+            }
+        } else {
+            return { cart: [...state.cart, { ...pizza, quantity: 1 }] }
         }
-    )
+    }),
+    increaseQuantity: (id) => set((state) => ({
+        cart: state.cart.map((item) =>
+            item.id === id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+        ),
+    })),
+    decreaseQuantity: (id) => set((state) => ({
+        cart: state.cart.map((item) =>
+            item.id === id && item.quantity > 1
+                ? { ...item, quantity: item.quantity - 1 }
+                : item
+        ),
+    })),
+    removeFromCart: (id) => set((state) => ({
+        cart: state.cart.filter((item) => item.id !== id)
+    })),
+    clearCart: () => set({ cart: [] }),
+})
+
+
+export const useStore = create(
+    persist(store, { name: 'cart-storage' })
 )
